e8780b0f5e5655ab59ee9384217699d7f02a8a73
Raconeur <cameronemrie1@gmail.com>
2022-06-21 16:08:15 -0500

    Finished Strings

--- Strings.py
+++ Strings.py
@@ -1,28 +1,66 @@
 string1 = "Hello"
+char = 'H'
 
 print(f'{string1}')
 print()
 
 # Substrings
 
-string = "waiter"
+string = "Waiter"
 
 num3 = string[2]
 print(f'This is the string in position 2: {num3}')
 print()
 
-numsTo3 = string[:3]
-print(f'These are strings in the positions 3 and below: {numsTo3}')
+charsTo3 = string[:3]
+print(f'These are strings in the positions 3 and below: {charsTo3}')
 print()
 
-numsPast3 = string[3:]
-print(f'These are strings in position 3 and above: {numsPast3}')
+charsPast3 = string[3:]
+print(f'These are strings in position 3 and above: {charsPast3}')
 print()
 
-num5 = numsPast3[1]
-print(f'This is the string in position 1: {num5}')
+char5 = charsPast3[1]
+print(f'This is the string in position 1: {char5}')
 print()
 
-lastNum = string[-1]
-print(f'Last char in the string: {lastNum}')
+lastChar = string[-1]
+print(f'Last char in the string: {lastChar}')
+print()
+
+negChar = string[:-3]
+print(f'Neg chars up to 4: {negChar}')
+print()
+
+slicedAddString = string[0:3] * 5
+print(f'This is our sliced add string: {slicedAddString}')
+print()
+
+# Cases with strings
+
+print(f'String lowercase: {string.lower()}')
+print()
+
+print(f'String uppercase: {string.upper()}')
+print()
+
+word = string + " and another waiter"
+print(f'String as title: {word.title()}')
+print()
+
+print(f'String swapcase: {string.swapcase()}')
+print()
+
+# Managing strings
+
+print(f'Letter W count: {word.count("W")}')
+print()
+
+print(f'Ends with er: {word.endswith("er")}')
+print()
+
+print(f'Ends with ed: {word.endswith("ed")}')
+print()
+
+print(f'ai is in position: {word.find("ai")}')
 print()

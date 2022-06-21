e440f2f924b20ca058accc03d644ea36a5480ebb
Raconeur <cameronemrie1@gmail.com>
2022-06-21 17:13:53 -0500

    Added Ifs and Inputs

---
+++ InputsAndIfs.py
@@ -0,0 +1,44 @@
+inp = input(f'Choose anything: ')
+print(f'Your inp is: {inp}')
+print()
+
+realInt = int(input(f'Choose an integer: '))
+print(f'This is our int: {realInt}')
+print()
+
+realString = str(input(f'Choose a string: '))
+print(f'This is our string: {realString}')
+print()
+
+realFloat = float(input(f'Choose a float: '))
+print(f'This is our float: {realFloat}')
+print()
+
+num = int(input(f'Choose a number: '))
+print()
+
+if (num == 5):
+    print(f'{num} is 5 :>')
+else:
+    print(f'{num} is not 5 :<')
+    
+if (num > 5):
+    print(f'{num} is greater than 5!')
+else:
+    print(f'{num} is not greater than 5')
+    
+if (num < 5):
+    print(f'{num} is less than 5')
+else:
+    print(f'{num} is not less than 5')
+    
+print()
+    
+string = str(input(f'Choose a string: '))
+if (string.find("f") > -1):
+    print(f'f is in the string')
+        
+
+
+    
+    

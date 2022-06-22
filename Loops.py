614143bc85c46d105e452c334c8541a5e9fd6416
Raconeur <cameronemrie1@gmail.com>
2022-06-21 23:21:00 -0500

    General Updates and addedlist

--- Loops.py
+++ Loops.py
@@ -1,4 +1,31 @@
 x = 0
-while (x <= 10):
-    print(x)
-    x = x + 1
+while (x < 5):
+    print(f'Woo!')
+    x += 1
+print()    
+
+
+z = 10
+while (z <= 10):
+    print(z)
+    if (z == 3):
+        break 
+    z -= 1
+print()
+
+run = True
+myString = "a"
+while (run):
+    if (len(myString) == 5):
+        print(myString)
+        run = False
+    myString += "a"
+print()
+    
+# OR 
+
+aString = "a"
+while (len(aString) != 5):
+    aString += "a"
+print(aString)
+print()

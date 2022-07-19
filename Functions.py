69ba1503a378f2695a37a77a5cc4f7c903450be4
Cam <cameronemrie1@gmail.com>
2022-07-19 14:42:26 -0500

    Modified Functions

--- Functions.py
+++ Functions.py
@@ -21,4 +21,12 @@ def findA(word:str):
 
 print(f'A is at {findA("dogsa")}')
 
-    
+def getFirst(aList:list):
+    return (aList[0])
+
+def getFirstNon(aList):
+    return (aList[0])
+
+oneList = ["f","x","z","u","p"]
+
+print(f'{getFirst(oneList)}')
